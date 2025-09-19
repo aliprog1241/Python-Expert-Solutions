8e4e6bcb64f89b8ee5694a5873e1d8b1326f56b6
Ali Miri <miri03369@gmail.com>
2025-09-19 17:40:26 +0330

    add coding

--- account/models.py
+++ account/models.py
@@ -1,11 +1,18 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Team(models.Model):
-    name = models.CharField(max_length=20)
-    jitsi_url_path = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+    zoom_url = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
 
 
-class Account(User):
-    team = models.ForeignKey(Team, null=True, on_delete=models.CASCADE)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.user.username
